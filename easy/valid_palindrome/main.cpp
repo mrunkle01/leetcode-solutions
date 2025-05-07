ed6dc66bf4d3bb2ca5617a40187b2122e5463fb3
mark <markrunkle67@gmail.com>
2025-05-06 21:09:24 -0500

    Valid Palindrom CPP Solution

---
+++ easy/valid_palindrome/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int lp =0; int rp = s.length() - 1;
+        while(lp<=rp){
+            if (!isalnum(s[lp])) {
+                lp++;
+                continue;
+            }
+            if (!isalnum(s[rp])) {
+                rp--;
+                continue;
+            }
+            if (tolower(s[lp]) != tolower(s[rp])) {
+                return false;
+            }
+            lp++;
+            rp--;
+        }
+        return true;
+    }
+};

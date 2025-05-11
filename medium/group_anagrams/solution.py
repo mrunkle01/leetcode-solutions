101b646fb914ed6baff4881cd612345227e78bf1
mark <markrunkle67@gmail.com>
2025-05-11 16:26:13 -0500

    Group Anagrams Py Solution

---
+++ medium/group_anagrams/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hm = {}
+        for i in strs:
+            s = tuple(sorted(i))
+            if s in hm:
+                hm[s].append(i)
+            else:
+                hm[s] = [i]
+        return list(hm.values())
+
+
+
+
+
+

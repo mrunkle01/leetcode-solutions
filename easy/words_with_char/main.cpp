472615d5500b92a41711a249d5e51240a91aaedb
mark <markrunkle67@gmail.com>
2025-05-26 14:16:50 -0500

    Words with Character CPP

---
+++ easy/words_with_char/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res = {};
+        for ( int i = 0; i < words.size(); i++ ){
+            if (words[i].find(x) != -1){
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
